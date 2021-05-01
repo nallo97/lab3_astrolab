d12892beffe091d072f29a8aeee00c25ff7929dd
Prak <8682372+pecom@users.noreply.github.com>
2021-05-01 11:55:02 -0400

    slow and bad anacode

--- anaslow.py
+++ anaslow.py
@@ -4,13 +4,15 @@ import healpy as hp
 import pickle
 from scipy import special
 
-fin_nside = 128
-
+fin_nside = 256
 
 fullmap = fits.open('./data/COM_CMB_IQU-smica_2048_R3.00_hm1.fits')
 idata = fullmap[1].data["I_STOKES"]
 print("Loaded data")
 ring_datai = hp.reorder(idata, inp='NESTED', out='RING')
+
+
+slow=False
 
 lesspretty = hp.ud_grade(ring_datai, fin_nside)
 
@@ -28,14 +30,26 @@ A_lm = []
 lmax = 3*fin_nside - 1
 print("lmax = ", lmax)
 
+npix = hp.nside2npix(fin_nside)
+
 for l in range(lmax):
     print("On l:", l)
     lilm = []
-#     maxl = min(l, 10)
-    for m in range(-l, l+1):
-        mval = np.sum(np.conj(special.sph_harm(m, l, anglist[:,0], anglist[:,1]))*lesspretty)
-        lilm.append(mval)
+    maxl = min(l, 10)
+    if slow:
+        for m in range(0, maxl+1):
+            mval = np.sum(np.conj(special.sph_harm(m, l, anglist[:,1], anglist[:,0]))*lesspretty*4*np.pi/npix)
+            lilm.append(mval)
+    else:
+        for m in range(0, l+1):
+            mval = np.sum(np.conj(special.sph_harm(m, l, anglist[:,1], anglist[:,0]))*lesspretty*4*np.pi/npix)
+            lilm.append(mval)
     A_lm.append(lilm)
 
-with open("./data/calc_alms_"+str(fin_nside), 'wb') as f:
+if slow:
+    fname = "./data/calc_alms_"+str(fin_nside)+'_slow'
+else:
+    fname = "./data/calc_alms_"+str(fin_nside)+'_bad'
+    
+with open(fname, 'wb') as f:
     pickle.dump(A_lm, f)
